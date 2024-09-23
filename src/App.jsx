fe3e7d5e3e97ce9d2f63041732ba74f8d364e864
adedejitobi055 <adedejiadeniyi055@gmail.com>
2024-09-23 13:38:20 +0100

    update contact me page

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Contact from "./pages/Contact";
 import { DarkModeProvider } from "./context/DarkModeContext";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
@@ -26,6 +27,23 @@ function App() {
             <Route path="*" element={<PageNotFound />} />
           </Route>
         </Routes>
+
+        <Toaster
+          position="top-right"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: { duration: 3000 },
+            error: { duration: 5000 },
+            style: {
+              fontSize: "15px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+              backgroundColor: "var(--color-grey-0)",
+              color: "var(--color-grey-700)",
+            },
+          }}
+        />
       </BrowserRouter>
     </DarkModeProvider>
   );
--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -2,8 +2,11 @@ import styled from "styled-components";
 import Input from "../../ui/Input";
 import Textarea from "../../ui/Textarea";
 import Button from "../../ui/Button";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import axios from "axios";
 
-const Container = styled.div`
+const Form = styled.form`
   display: flex;
   flex-direction: column;
   gap: 4rem;
@@ -16,15 +19,61 @@ const Container = styled.div`
 `;
 
 function ContactForm() {
+  const [formData, setFormData] = useState({
+    Name: "",
+    Email: "",
+    Message: "",
+  });
+
+  //   const [status, setStatus] = useState("");
+
+  const scriptURL =
+    "https://script.google.com/macros/s/AKfycby5TvivWYQ0S29_KKFJxFyArYAJJgSqTdAoD-46b7s/dev";
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(scriptURL, formData, {
+        headers: { "Content-Type": "application/json" },
+      });
+      toast.success("Message sent successfully", response.data);
+    } catch (error) {
+      console.log("Error sending message: ", error);
+      toast.error("Error sending message. Please try again.");
+    }
+  };
+
   return (
-    <>
-      <Container>
-        <Input name="name" type="text" placeholder="Your name" />
-        <Input name="email" type="text" placeholder="Your email" />
-        <Textarea name="message" placeholder="Your message" />
-        <Button type="submit">Submit</Button>
-      </Container>
-    </>
+    <Form onSubmit={handleSubmit}>
+      <Input
+        name="Name"
+        type="text"
+        placeholder="Your name"
+        required
+        value={formData.Name}
+        onChange={handleChange}
+      />
+      <Input
+        name="Email"
+        type="email"
+        placeholder="Your email"
+        required
+        value={formData.Email}
+        onChange={handleChange}
+      />
+      <Textarea
+        name="Message"
+        placeholder="Your message"
+        required
+        value={formData.Message}
+        onChange={handleChange}
+      />
+      <Button type="submit">Send Message</Button>
+    </Form>
   );
 }
 
