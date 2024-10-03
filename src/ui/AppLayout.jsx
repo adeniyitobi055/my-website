be48a3eb688da1faed689ae92810ce554396e2e7
adedejitobi055 <adedejiadeniyi055@gmail.com>
2024-10-03 15:12:02 +0100

    update page movement

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -6,14 +6,14 @@ import { useSwipeNavigation } from "../hooks/useSwipeNavigation";
 const StyledAppLayout = styled.div`
   display: flex;
   flex-direction: column;
-  height: 100vh;
+  height: 100%;
 `;
 
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 13rem 4.8rem 6.4rem;
   overflow: auto;
-  height: 100vh;
+  height: 100%;
 
   @media screen and (max-width: 425px) and (min-width: 320px) {
     padding: 10.5rem 2.5rem 3.2rem;
