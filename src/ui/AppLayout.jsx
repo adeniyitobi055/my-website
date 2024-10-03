21a739d179877f4e50486ea505ff19dfedb49419
adedejitobi055 <adedejiadeniyi055@gmail.com>
2024-10-03 15:08:14 +0100

    update page movement

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -14,7 +14,7 @@ const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 13rem 4.8rem 6.4rem;
   overflow: auto;
-  height: 100%;
+  height: 100vh;
 
   @media screen and (max-width: 425px) and (min-width: 320px) {
     padding: 10.5rem 2.5rem 3.2rem;
