8ee2549cb52f9ee007e8364d8535e7ec95e8df30
adedejitobi055 <adedejiadeniyi055@gmail.com>
2024-10-07 11:16:28 +0100

    update scroll

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -18,7 +18,7 @@ const Main = styled.main`
   margin-top: 7rem;
 
   @media screen and (max-width: 425px) {
-    padding: 10.5rem 2.5rem 3.2rem;
+    padding: 10.5rem 2.5rem 0;
     margin-top: 6rem;
   }
 
